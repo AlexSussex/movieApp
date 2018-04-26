f15fa08609d608a62a537f9dd4ccd2b91e8d62f9
AlexSussex <ap576@sussex.ac.uk>
2018-04-26 12:04:26 +0100

    delete movie feature

--- src/main/java/com/qa/integration/MovieEndpoint.java
+++ src/main/java/com/qa/integration/MovieEndpoint.java
@@ -2,6 +2,7 @@ package com.qa.integration;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -36,5 +37,12 @@ public class MovieEndpoint {
 	@Produces({ "application/json" })
 	public String createMovie(String jsonString) {
 		return service.createMovie(jsonString);
+	}
+	
+	@DELETE
+	@Path("/json/{id}")
+	@Produces({ "application/json" })
+	public String deleteMovie(@PathParam ("id") Long id) {
+		return service.deleteMovie(id);
 	}
 }
